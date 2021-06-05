ff2b326a6dab6e1804694952bbd7ace2bad81ab2
Andrey772005 <45210596+Andrey772005@users.noreply.github.com>
2021-06-05 15:44:28 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,13 @@
-
 function randomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if (min <= 0 && max >= 100) {
-    console.log('Введено неверное число');
-  } 
+  if (min === max || max < min) {
+    return min;
+  }
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-randomNumber(1, 3);
+}
+console.log(randomNumber(20, 30));
 
 const MAX_STRING_LENGTH = 140;
-function isValidMaxStringLength(someComment, maxLength = MAX_STRING_LENGTH) {
-  if (someComment.length > maxLength) {
-  return true;
-  } else {
-  return false;
-  }
-};
-
-//const string = 'Some too long comment';
-//console.log(isValidMaxStringLength(string));
-
-
-
-
+const isValidMaxStringLength = (someComment, maxLength = MAX_STRING_LENGTH) =>
+  someComment.length <= maxLength;
