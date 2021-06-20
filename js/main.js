498d5bfc245ffc8f28699f04dc6d4501bc4ded30
Andrey772005 <45210596+Andrey772005@users.noreply.github.com>
2021-06-20 13:27:46 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,39 +7,9 @@ function randomNumber (min, max) {
   return Math.floor(result);
 }
 
-
 // const MAX_STRING_LENGTH = 140;
 // const isValidMaxStringLength = (someComment, maxLength = MAX_STRING_LENGTH) =>
 //   someComment.length <= maxLength;
-
-
-const PHOTO_URL = [
-  'photos/1.jpg',
-  'photos/2.jpg',
-  'photos/3.jpg',
-  'photos/4.jpg',
-  'photos/5.jpg',
-  'photos/6.jpg',
-  'photos/7.jpg',
-  'photos/8.jpg',
-  'photos/9.jpg',
-  'photos/10.jpg',
-  'photos/11.jpg',
-  'photos/12.jpg',
-  'photos/13.jpg',
-  'photos/14.jpg',
-  'photos/15.jpg',
-  'photos/16.jpg',
-  'photos/17.jpg',
-  'photos/18.jpg',
-  'photos/19.jpg',
-  'photos/20.jpg',
-  'photos/21.jpg',
-  'photos/22.jpg',
-  'photos/23.jpg',
-  'photos/24.jpg',
-  'photos/25.jpg',
-];
 
 const DESCRIPTION = [
   'Бухта у отеля',
@@ -67,15 +37,6 @@ const DESCRIPTION = [
   'Краб',
   'Нашествие',
   'Сафари',
-];
-
-const AVATAR = [
-  'img/avatar-1.svg',
-  'img/avatar-2.svg',
-  'img/avatar-3.svg',
-  'img/avatar-4.svg',
-  'img/avatar-5.svg',
-  'img/avatar-6.svg',
 ];
 
 const MESSAGE = [
