7ee2a2d75ae0d5c8a4d6de5c4971c62fb8c1fe53
Andrey772005 <45210596+Andrey772005@users.noreply.github.com>
2021-07-02 21:26:17 +0300

    Обновил main.js 02.07.21

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 import {dataGenerator} from './data.js';
 import './user_photo.js';
 import './user_photo_full_size.js';
-import './validation_form.js';
-import './utils.js';
-import './data.js';
 
 console.log(dataGenerator(25));
