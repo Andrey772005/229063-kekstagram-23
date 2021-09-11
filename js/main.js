db8c46a51ae73638bf4cd867ac30060e173f739b
Andrey772005 <45210596+Andrey772005@users.noreply.github.com>
2021-09-11 21:18:47 +0300

    Update master 11.09.21

--- js/main.js
+++ js/main.js
@@ -1,51 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable id-length */
 function getRandomPositiveInteger (a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-};
+}
 
 function checkStringLength (string, length) {
   return string.length <= length;
 }
 
-// В файле main.js на основе написанных по заданию ранее вспомогательных функций напишите необходимые функции для создания массива из 25
-//  сгенерированных объектов. Каждый объект массива — описание фотографии, опубликованной пользователем.
-
-// Структура каждого объекта должна быть следующей:
-
-// id, число — идентификатор описания. Это число от 1 до 25. Идентификаторы не должны повторяться.
-
-// url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
-
-// description, строка — описание фотографии. Описание придумайте самостоятельно.
-
-// likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
-
-// comments, массив объектов — список комментариев, оставленных другими пользователями к этой фотографии.
-//  Количество комментариев к каждой фотографии вы определяете на своё усмотрение. Все комментарии генерируются случайным образом.
-// Пример описания объекта с комментарием:
-
-// {
-//   id: 135,
-//   avatar: 'img/avatar-6.svg',
-//   message: 'В целом всё неплохо. Но не всё.',
-//   name: 'Артём',
-// }
-// У каждого комментария есть идентификатор — id — случайное число. Идентификаторы не должны повторяться.
-
-// Поле avatar — это строка, значение которой формируется по правилу img/avatar-{{случайное число от 1 до 6}}.svg.
-//  Аватарки подготовлены в директории img.
-
-// Для формирования текста комментария — message — вам необходимо взять одно или два случайных предложения из представленных ниже:
-
-// Всё отлично!
-// В целом всё неплохо. Но не всё.
-// Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.
-// Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.
-// Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.
-// Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!
-// Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.
 
 const GENERATE_PHOTO = 25;
 
@@ -59,7 +24,7 @@ const NAMES = [
   'Вячеслав',
   'Наталья',
   'Ростислав',
-  'Женя'
+  'Женя',
 ];
 
 const COMMENTS = [
@@ -69,7 +34,7 @@ const COMMENTS = [
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-  'Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.'
+  'Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.',
 ];
 
 const DESCRIPTION = [
@@ -110,20 +75,19 @@ const commentsGenerator = (commentObj) => {
       message: COMMENTS.slice(0, getRandomPositiveInteger(0, COMMENTS.length - 1)).join(''),
       name: NAMES[getRandomPositiveInteger(0, NAMES.length - 1)],
     };
-  };
+  }
 
   return comments;
 };
 
-const photoObj = function () {
-  return {
-    id: getRandomPositiveInteger(1, 25),
-    url: `photos/${ getRandomPositiveInteger(1, 25) }.jpg`,
-    description: DESCRIPTION[getRandomPositiveInteger(0, DESCRIPTION.length - 1)],
-    likes: getRandomPositiveInteger(15, 200),
-    comments: commentsGenerator(getRandomPositiveInteger(0, 10))
-  }
-};
+const photoObj = () => ({
+  id: getRandomPositiveInteger(1, 25),
+  url: `photos/${ getRandomPositiveInteger(1, 25) }.jpg`,
+  description: DESCRIPTION[getRandomPositiveInteger(0, DESCRIPTION.length - 1)],
+  likes: getRandomPositiveInteger(15, 200),
+  comments: commentsGenerator(getRandomPositiveInteger(0, 10)),
+});
 
 
 const generatePhotoObj = new Array(GENERATE_PHOTO).fill(null).map(() => photoObj());
+
