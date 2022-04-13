3b55814eb32f8a36eca666361548bac2c58de848
Andrey772005 <45210596+Andrey772005@users.noreply.github.com>
2022-04-13 12:08:01 +0300

    Нужно больше функций 13.04.2022

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
-/* eslint-disable no-unused-vars */
-import {generatePhotoObj} from './generate-data.js';
+// eslint-disable-next-line no-unused-vars
+function getRandomPositiveInteger (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+// eslint-disable-next-line no-unused-vars
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
